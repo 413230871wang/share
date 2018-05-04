6413ac434f6f796d907bcae51bb8f63306fa4cf9
coder_wanglei <wanglei19930915@126.com>
2018-05-04 14:31:51 +0800

    上传share项目

---
+++ src/main/java/com/lay/ShareyourselfApplication.java
@@ -0,0 +1,12 @@
+package com.lay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareyourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareyourselfApplication.class, args);
+	}
+}
